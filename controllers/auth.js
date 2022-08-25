872325213eac0ca2b9e0db00c4dd01fb0b98d0d4
Emiliano Tognocchi <etognocchi@koandina.com>
2022-08-25 10:52:51 -0300

    Google Sing add

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async ( req , res = response ) => {
 
@@ -50,6 +51,55 @@ const login = async ( req , res = response ) => {
 
 }
 
+const googleSingIn = ( req, res = response ) => {
+    const { id_token } = req.body;
+
+    try {
+        
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne( { correo } );
+
+        if(!usuario){
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: '123',
+                img,
+                google: true
+            }
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // Si el usaurio en BD
+        if ( !usuario.estado ){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            msg: 'OK',
+            usuario,
+            token
+        })    
+
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar.'
+        })
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
